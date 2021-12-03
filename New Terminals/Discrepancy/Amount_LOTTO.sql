b8d1813da541c0130272fe64021509c4a58658d6
105864 <henrique.m.corte@scml.pt>
2021-12-03 17:34:30 +0000

    query of Hades/Hera to compare Sales Data values with the MILL system when there were probleams of discrepancy of draw days (SHAREUPD - shares total in fault).

--- New Terminals/Discrepancy/Amount_LOTTO.sql
+++ New Terminals/Discrepancy/Amount_LOTTO.sql
@@ -2,24 +2,24 @@
 
 --select TO_TIMESTAMP('21.11.02 00:00:00','YY-MM-DD HH24::SS') as time
 
-select sum(wager_amount) as total_LOTTO,count(wager_id) AS wagers_count
-FROM tl_t_wager 
-WHERE 
-wager_date >= TO_TIMESTAMP('21.11.02 00:00:00','YY-MM-DD HH24::SS') AND 
-wager_date <= TO_TIMESTAMP('21.11.02 23:59:59','YY-MM-DD HH24::SS') AND
-CANCEL_DATE IS NULL AND --CANCEL_SERIAL_NUMBER IS NOT NULL AND
-draw_number = 88 --88 (Totoloto de Quarta) --89 (Totoloto de Sabado)
+--select sum(wager_amount) as total_LOTTO,count(wager_id) AS wagers_count
+--FROM tl_t_wager 
+--WHERE 
+--wager_date >= TO_DATE('21.11.02 00:00:00','YY-MM-DD HH24:MI:SS') AND 
+--wager_date < TO_DATE('21.11.03 00:00:00','YY-MM-DD HH24:MI:SS') AND
+--CANCEL_DATE IS NULL AND --CANCEL_SERIAL_NUMBER IS NOT NULL AND
+--draw_number = 88 --88 (Totoloto de Quarta) --89 (Totoloto de Sabado)
 
 --                          COUNT            TOTAL AMT
--- TOTAL Sales              168              995,4  (Hades query a tl_t_wager draw_number=88)
+-- TOTAL Sales              147              693    (Hades query a tl_t_wager draw_number=88)
 -- Total sales              147              693.00 (MILL Totoloto de Quarta GIND=4) --> LOTQ-02-11-2021.DAT;1
--- Conclução discrepância   21 apostas       302,4 euros 
+-- Conclução os valores correspodem correctamente
 
 --select sum(wager_amount) as total_LOTTO,count(wager_id) AS wagers_count
 --FROM tl_t_wager 
 --WHERE 
---wager_date >= '21.11.02 00:00:00' AND 
---wager_date <= '21.11.02 23:59:59' AND
+--wager_date >= TO_DATE('21.11.02 00:00:00','YY-MM-DD HH24:MI:SS') AND 
+--wager_date < TO_DATE('21.11.03 00:00:00','YY-MM-DD HH24:MI:SS') AND
 --CANCEL_DATE IS NULL AND --CANCEL_SERIAL_NUMBER IS NOT NULL AND
 --draw_number = 89 -- 88 (Totoloto de Quarta) --89 (Totoloto de Sabado)
 
@@ -27,22 +27,22 @@ draw_number = 88 --88 (Totoloto de Quarta) --89 (Totoloto de Sabado)
 -- TOTAL Sales              126              5890,5  (Hades query a tl_t_wager draw_number=89) (wagers total without taking into account cancellations)
 -- TOTAL Sales              119              5884,2  (Hades query a tl_t_wager draw_number=89)
 -- Total sales              119              5884.20 (MILL Totoloto de Quarta GIND=4) --> LOTS-02-11-2021.DAT;2
--- Conclução os valores batem correctamente
+-- Conclução os valores correspodem correctamente
 
 -------------------------- SPORTS ----------------------------------------------------------------------------------------------
 
---select sum(wager_amount) as total_LOTTO,count(wager_id) AS wagers_count
---FROM tb_t_wager 
---WHERE 
---wager_date >= '21.11.02 00:00:00' AND 
---wager_date <= '21.11.02 23:59:59' AND
---CANCEL_DATE IS NULL AND --CANCEL_SERIAL_NUMBER IS NOT NULL AND
---draw_number = 45
+select sum(wager_amount) as total_LOTTO,count(wager_id) AS wagers_count
+FROM tb_t_wager 
+WHERE 
+wager_date >= TO_DATE('21.11.02 00:00:00','YY-MM-DD HH24:MI:SS') AND 
+wager_date < TO_DATE('21.11.03 00:00:00','YY-MM-DD HH24:MI:SS') AND
+CANCEL_DATE IS NULL AND --CANCEL_SERIAL_NUMBER IS NOT NULL AND
+draw_number = 45
 
 --                           COUNT           TOTAL AMT
 -- Total sales               49              147.00      (MILL  totobola normal)
 -- Total sales               49              147         (Hades totobola normal)      --> TOTO-02-11-2021.DAT;2
-
+-- Conclução os valores correspodem correctamente
 
 --rownum <= 10
 
